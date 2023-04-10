f6cd3be8c0db9d2b2c21a550ed602079d52d9533
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-04-11 01:06:34 +0900

    Modify the build script to use submodules

--- gz-msgs/build.rs
+++ gz-msgs/build.rs
@@ -16,7 +16,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut protos = vec![];
 
-    for entry in fs::read_dir("proto/gz/msgs")? {
+    for entry in fs::read_dir("3rdparty/gz-msgs/proto/gz/msgs")? {
         let path = entry?.path();
         if path.is_file() && path.extension() == Some("proto".as_ref()) {
             let path_str = path.to_str().unwrap();
@@ -28,7 +28,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let ret = Codegen::new()
         .out_dir("./src/msgs")
         .inputs(&protos)
-        .include("proto")
+        .include("3rdparty/gz-msgs/proto")
         .customize_callback(GenGzMessage)
         .run();
 
