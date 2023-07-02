c462da8f05bcdca5171069bb5549e46a3be1d5ca
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-07-02 21:05:46 +0900

    Support fortress (#5)

--- gz-transport-sys/build.rs
+++ gz-transport-sys/build.rs
@@ -1,13 +1,25 @@
 use std::{env, error::Error};
 
-use pkg_config::Config;
+use pkg_config::{Config, Library};
+
+fn find_library() -> Result<Library, &'static str> {
+    let candidates = ["gz-transport12", "ignition-transport11"];
+
+    for candidate in candidates {
+        if let Ok(library) = Config::new().probe(candidate) {
+            return Ok(library);
+        }
+    }
+
+    Err("gz-transport or ignition-transport not found")
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     if env::var("DOCS_RS").is_ok() {
         return Ok(());
     }
 
-    let library = Config::new().probe("gz-transport12")?;
+    let library = find_library()?;
 
     for path in library.link_paths.iter() {
         println!("cargo:rustc-link-search=native={}", path.to_str().unwrap());
