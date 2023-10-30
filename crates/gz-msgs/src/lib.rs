602d988a3f87d92ac3b14ffd0f0ebb74c83862b5
Yuma Hiramatsu <yuma.hiramatsu@gmail.com>
2023-10-31 02:35:45 +0900

    Remove unnecesary condition

--- crates/gz-msgs/src/lib.rs
+++ crates/gz-msgs/src/lib.rs
@@ -10,19 +10,19 @@
 #[cfg_attr(docsrs, doc(cfg(feature = "fortress")))]
 /// [Gazebo Message 8.7.0](https://github.com/gazebosim/gz-msgs/tree/ignition-msgs8_8.7.0)
 pub mod ign_msgs8;
-#[cfg(all(feature = "fortress", not(docsrs)))]
+#[cfg(feature = "fortress")]
 pub use ign_msgs8::*;
 
 #[cfg(any(feature = "garden", feature = "generate", docsrs))]
 #[cfg_attr(docsrs, doc(cfg(feature = "garden")))]
 /// [Gazebo Message 9.5.0](https://github.com/gazebosim/gz-msgs/tree/gz-msgs9_9.5.0)
 pub mod gz_msgs9;
-#[cfg(all(feature = "garden", not(docsrs)))]
+#[cfg(feature = "garden")]
 pub use gz_msgs9::*;
 
 #[cfg(any(feature = "harmonic", feature = "generate", docsrs))]
 #[cfg_attr(docsrs, doc(cfg(feature = "harmonic")))]
 /// [Gazebo Message 10.0.0](tps://github.com/gazebosim/gz-msgs/tree/gz-msgs10_10.0.0)
 pub mod gz_msgs10;
-#[cfg(all(feature = "harmonic", not(docsrs)))]
+#[cfg(feature = "harmonic")]
 pub use gz_msgs10::*;
